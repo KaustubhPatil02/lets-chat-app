06087375b37f4eedda3a5d593a9f5d0be5be2732
Kaustubh Patil <patilkaustubh990@gmail.com>
2024-10-22 19:39:05 +0530

    updated ui and removed sensentive data to env

--- src/COMPO/Navbar.jsx
+++ src/COMPO/Navbar.jsx
@@ -1,23 +1,31 @@
-import { signOut } from 'firebase/auth'
-import React, { useContext } from 'react'
-import { auth } from '../firebase'
-import{ AuthContext } from '../context/AuthContext'
+import { signOut } from 'firebase/auth';
+import React, { useContext } from 'react';
+import { auth } from '../firebase';
+import { AuthContext } from '../context/AuthContext';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 const Navbar = () => {
-  const {currentUser} = useContext(AuthContext)
+  const { currentUser } = useContext(AuthContext);
+
+  // Function to toggle sidebar visibility
+  const handleHamburgerClick = () => {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.toggle('sidebar-collapsed');
+  };
+
   return (
-    <div className='navbar'> 
-    <span className='logo'>Let's Chat</span>
-    <div className="user">
+    <div className='navbar'>
+      <GiHamburgerMenu className="hamburger-icon" onClick={handleHamburgerClick} />
+      <span className='logo'>Let's Chat</span>
+      <div className="user">
         <img src={currentUser.photoURL} alt="" />
         <span>{currentUser.displayName}</span>
-        <button onClick={()=>
-          signOut(auth)}>
-            Logout
+        <button onClick={() => signOut(auth)}>
+          Logout
         </button>
-    </div>  
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/firebase.js
+++ src/firebase.js
@@ -4,16 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAkua2sKkNaLpDumq7c5JzurvTHRj93LYI",
-  authDomain: "lets-chat-app-13951.firebaseapp.com",
-  projectId: "lets-chat-app-13951",
-  storageBucket: "lets-chat-app-13951.appspot.com",
-  messagingSenderId: "156880776325",
-  appId: "1:156880776325:web:625d83957e30c367b24dec"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
-
-
-
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
